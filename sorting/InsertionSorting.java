add9acb1fdd3ce2f4aa053cc57ca8469b5a8fe55
Andrés Botía <anboca@gmail.com>
2021-01-01 13:51:31 -0500

    Insertion Sorting

---
+++ sorting/InsertionSorting.java
@@ -0,0 +1,37 @@
+public class InsertionSorting {
+
+    /*
+    * Created by: Andrés Botía Carreño
+    * Year: 2021
+    * Licence: GNU V3
+    * Description: Insertion Sorting Algorithm
+    * Complexity: O(n^2)
+    * */
+
+    public static void main(String[] args){
+        int[] array={5,2,4,6,1,3};
+        printArray(array);
+        array = insertionSorting(array);
+        printArray(array);
+    }
+    public static void printArray(int[] array){
+        for (int i=0;i<array.length;i++)
+            System.out.print(" "+array[i]);
+        System.out.println();
+    }
+
+    public static int[] insertionSorting(int[] array){
+        int i, j, key;
+        for (i=1;i< array.length;i++){
+            key = array[i];
+            j = i-1;
+            while (j>=0 && array[j]>key){
+                array[j+1]=array[j];
+                j--;
+            }
+            array[j+1]=key;
+        }
+        return array;
+    }
+
+}
